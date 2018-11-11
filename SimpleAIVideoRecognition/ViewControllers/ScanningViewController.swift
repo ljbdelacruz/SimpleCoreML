3432dd70e4c887db0e61cea8bd88b865ab6191d6
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2018-11-11 08:11:17 +0800

    Optimize Codes

--- SimpleAIVideoRecognition/ViewControllers/ScanningViewController.swift
+++ SimpleAIVideoRecognition/ViewControllers/ScanningViewController.swift
@@ -15,13 +15,38 @@ import Vision
 class ScanningViewController: UIViewController {
     @IBOutlet weak var UIVideoDisplay: PreviewView!
     @IBOutlet weak var UIItemDescription: UILabel!
+    // ML - Initialization
+    var model: VNCoreMLModel?
+    
     
     private let session = AVCaptureSession()
     private let sessionQueue = DispatchQueue(label: "Camera Session Queue", attributes: [], target: nil)
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.UIVideoDisplay.videoPreviewLayer.videoGravity = AVLayerVideoGravity.resize
+        self.UIVideoDisplay.session = session
+        
+        self.checkPermission(completionHandler: {
+            (isGranted) in
+            if isGranted {
+                self.SetupCamera();
+                self.loadModel()
+            }else{
+                print("Sorry Camera not available");
+            }
+        })
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.sessionQueue.async {
+            self.session.startRunning()
+        }
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        self.sessionQueue.async { [unowned self] in
+            self.session.stopRunning()
+        }
+        super.viewWillDisappear(animated)
     }
 }
 
@@ -47,4 +72,49 @@ extension ScanningViewController{
             self.sessionQueue.resume()
         }
     }
+}
+//MARK: camera setup func
+extension ScanningViewController:AVCaptureVideoDataOutputSampleBufferDelegate{
+    func SetupCamera(){
+        self.session.beginConfiguration()
+        self.session.sessionPreset = .hd1280x720
+        
+        guard let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: AVMediaType.video, position: .back) else { return }
+        guard let captureDeviceInput = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        guard self.session.canAddInput(captureDeviceInput) else { return }
+        self.session.addInput(captureDeviceInput)
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "sample buffer"))
+        videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String : kCVPixelFormatType_32BGRA]
+        videoOutput.alwaysDiscardsLateVideoFrames = true
+        guard self.session.canAddOutput(videoOutput) else { return }
+        self.session.addOutput(videoOutput)
+        self.session.commitConfiguration()
+        videoOutput.setSampleBufferDelegate(self, queue: sessionQueue)
+    }
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        // TODO: Do ML Here
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        self.CheckResult(pixelBuffer: pixelBuffer);
+        
+    }
+}
+
+//MARK: CoreML Func
+extension ScanningViewController{
+    private func loadModel() {
+        model = try? VNCoreMLModel(for: Inceptionv3().model)
+    }
+    private func CheckResult(pixelBuffer:CVPixelBuffer){
+        let request = VNCoreMLRequest(model: model!) {
+            (finishedReq, err) in
+            guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
+            guard let firstObservation = results.first else { return }
+            DispatchQueue.main.async {
+                self.UIItemDescription.text=firstObservation.identifier.components(separatedBy: ",").first;
+            }
+        }
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+    
 }
